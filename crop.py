261abbc15b5825de0c744308040e1b7697104766
Beatriz Cepa <cepabeatriz@gmail.com>
2022-06-28 00:13:34 +0100

    create crop.py

---
+++ crop.py
@@ -0,0 +1,25 @@
+
+# Importing Image class from PIL module
+from PIL import Image
+import os
+
+def crop(height, width):
+    k=1
+    os.mkdir('C:/Users/user/desktop/cropped/')
+    dir=r'C:/Users/user/desktop/cropped/'
+    im = Image.open(r'C:/Users/user/desktop/files/result/dcgan1/preview/image00000100.png')
+    imgwidth, imgheight = im.size
+    # print(im.size)
+    for i in range(0,imgheight,height):
+        for j in range(0,imgwidth,width):
+            box = (j, i, j+width, i+height)
+            a = im.crop(box)
+            a.save(os.path.join(dir,"IMG-%s.png" % k))
+            # try:
+            #     # o = a.crop(area)
+            #     a.save(os.path.join(dir,"IMG-%s.png" % k))
+            # except:
+            #     pass
+            k +=1
+
+crop(256,256)
